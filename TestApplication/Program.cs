4617f29c04f392589395ef4946e206be8669b92f
lpemake <petteri.makela@seamk.fi>
2014-12-11 20:35:05 +0200

    Multiply call added

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -14,6 +14,7 @@ namespace TestApplication
             Console.WriteLine(MathLibrary.MathUtilities.Add(3, 4));
             Console.WriteLine(MathLibrary.MathUtilities.Add(3, 4));
             Console.WriteLine(MathLibrary.MathUtilities.Divide(3, 4));
+            Console.WriteLine(MathLibrary.MathUtilities.Multiply(3, 4));
         }
     }
 }
