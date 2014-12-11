40d70f87dcda7b92b25cd414f966d586936dff22
lpemake <petteri.makela@seamk.fi>
2014-12-11 19:14:13 +0200

    eka versio

---
+++ MathLibrary/MathUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class MathUtilities
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ TestApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathLibrary;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathLibrary.MathUtilities.Add(3, 4));
+            Console.WriteLine(MathLibrary.MathUtilities.Add(3, 4));
+        }
+    }
+}
