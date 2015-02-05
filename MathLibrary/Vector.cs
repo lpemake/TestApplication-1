17d0a3b9aff99add902496c2ad2bee5f5d574c80
lpemake <petteri.makela@seamk.fi>
2015-02-05 18:33:28 +0200

    dgdfg

---
+++ MathLibrary/Vector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    class Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Vector(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Y = z;
+        }
+    }
+}
