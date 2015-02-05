5520c0285b0d7e3c3d63bc87ace8b5ee57a7523d
lpemake <petteri.makela@seamk.fi>
2015-02-05 18:40:02 +0200

    cc

--- MathLibrary/Vector.cs
+++ MathLibrary/Vector.cs
@@ -12,6 +12,12 @@ namespace MathLibrary
         public double Y { get; set; }
         public double Z { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
         public Vector(double x, double y, double z)
         {
             this.X = x;
